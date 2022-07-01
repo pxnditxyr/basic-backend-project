07f1e47c537b607c0ff2aeec8a121b5a71c0297d
Yuki xD <pxnditxyr@gmail.com>
2022-06-30 20:06:22 -0400

    feat: Created basic connection to Database with sequelize and mysql2

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import { Server } from './models/server.js';
 
 dotenv.config();
+
 const server = new Server();
 
 server.listen();
--- models/server.js
+++ models/server.js
@@ -1,12 +1,22 @@
-import express from 'express'
+import express from 'express';
+import { db } from '../db/connection.js';
 
 export class Server {
     
     constructor () {
         this.app  = express();
         this.port = process.env.PORT || '4000';
-
+        
+        this.dbConnection();
         this.middlewares();
+    }
+
+    async dbConnection () {
+        try {
+            await db.authenticate();
+        } catch ( error ) {
+            throw new Error( error );
+        }
     }
 
     middlewares () {
